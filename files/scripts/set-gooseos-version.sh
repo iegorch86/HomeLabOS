29d99e153b1a13c033c004a03f652d893e0a9408
iegorch86 <66792505+iegorch86@users.noreply.github.com>
2026-07-18 20:19:13 -0400

    Update set-gooseos-version.sh
    Update sys line info

--- files/scripts/set-gooseos-version.sh
+++ files/scripts/set-gooseos-version.sh
@@ -40,7 +40,7 @@ fi
 
 RELEASE_CHANNEL="${RELEASE_TYPE:-stable}"
 
-PRETTY_NAME_VALUE="GooseOS ${EDITION} — build ${BUILD_DATE} — version ${BASE_VERSION} — ${RELEASE_CHANNEL}"
+PRETTY_NAME_VALUE="GooseOS ${EDITION}_build ${BUILD_DATE}_version ${BASE_VERSION}_${RELEASE_CHANNEL}"
 
 sed -i \
     -e 's/^NAME=.*/NAME="GooseOS"/' \
